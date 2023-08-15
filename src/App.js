7ae71418f3c006c3fd8687f2049cdd694adb7808
Abhishek Tiwari <tiwariabhishek27611@gmail.com>
2023-08-15 13:07:00 +0530

    done the initial folder and package setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./components/home/Home";
+import MovieDetails from "./components/movieDetails/MovieDetails";
+import movieApi from "./common/apis/movieApi";
+import { API_KEY } from "./common/apis/aipKeys";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/movie:imbdId" element={<MovieDetails />} />
+          </Switch>
+        </div>
+      </Router>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.scss";
+const Footer = () => {
+  return (
+    <div className="footer">
+      Footer
+      <div>Movie App</div>
+      <div>22023, Movie, in or it's affeliatiates</div>
+    </div>
+  );
+};
+
+export default Footer;
